00734378c32a3f1f0e008a349d75f0dc638ede34
Alex Kanatev <45164382+kanatev@users.noreply.github.com>
2023-11-21 00:07:54 +0100

    UI GameOver Canvas
    -added GameOver Canvas UI + logic

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -3,20 +3,17 @@ using System.Collections.Generic;
 using SkeletonEditor;
 using TMPro;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Sword swordWeapon;
     [SerializeField] private PlayerController playerHero;
+    [SerializeField] private GameObject gameOverCanvas;
     
     [Header("UI")]
     [SerializeField] private TMP_Text labelExpCounter;
-
     [SerializeField] private TMP_Text labelHealthCounter;
-    
-    
-
-    // private int expCounter = 0;
     
     // Start is called before the first frame update
     void Start()
@@ -29,5 +26,15 @@ public class GameManager : MonoBehaviour
     {
         labelExpCounter.SetText(swordWeapon.CollectedExp.ToString());
         labelHealthCounter.SetText(playerHero.HealthPoints.ToString());
+        if (playerHero.HealthPoints < 1)
+        {
+            Cursor.lockState = CursorLockMode.None;
+            gameOverCanvas.SetActive(true);
+        }
+    }
+
+    public void RestartButtonFunc()
+    {
+        SceneManager.LoadScene("Modular Dungeon");
     }
 }
